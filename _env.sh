496db1c45282116508a1aaae0693145b16b1417c
JGWill <jgi@jgwill.com>
2025-12-07 00:49:26 -0500

    redo the launcher #11

---
+++ _env.sh
@@ -0,0 +1,8 @@
+  
+# Namespace: Chimera_Storyform__TheWinterSolsticeCircle 
+session_id__Chimera_Storyform__TheWinterSolsticeCircle_2512070048=9eea453b-cad8-4069-b3ed-1748d252ad22
+alias cdChimera_Storyform__TheWinterSolsticeCircle="cd /src/_sessiondata/9eea453b-cad8-4069-b3ed-1748d252ad22 && ls -ltra && . session_bash_func_aliases.sh&>/dev/null" 
+alias cdPlansChimera_Storyform__TheWinterSolsticeCircle='cd /src/_sessiondata/9eea453b-cad8-4069-b3ed-1748d252ad22/plans 2>/dev/null || echo "No plans directory found."; ls -ltra; echo "Enter plan filename to open:"; read planfile; if [ -f "$planfile" ]; then nano "$planfile"; else echo "File does not exist."; fi' 
+session_id__Chimera_Storyform__TheWinterSolsticeCircle_2512070048__MCP_CONFIG="/src/.mcp.github.miadisabelle.json /src/.mcp.iaip.json /src/.mcp.lighthouse.json /src/.mcp.miawapascone.json"
+session_id__Chimera_Storyform__TheWinterSolsticeCircle_2512070048__ADD_DIR="/src/STPB/ /src/IAIP/ /cesaret/book/_/tcc/holistic_thinker/ /cesaret/book/_/tcc/winter_solstice/ /b/Dropbox/stories/winter_solstice/ /src/stories/the_winter_solstice/"
+  
